2bf60958fcdb72a97f1cb604c69684f7d11970f9
ariazero <aryaabadian@gmail.com>
2024-06-26 08:52:40 +0330

    reusable banner section component created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import Hero from "./components/Hero/Hero";
 import CategoryRow1 from "./components/Category/CategoryRow1";
 import CategoryRow2 from "./components/Category/CategoryRow2";
 import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+import headphone from './assets/hero/headphone.png'
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bas",
+  title3: "Winter Sale",
+  title4:
+    "Unmissable Offer on Ari Solo Bas Headphones! Dive into the world of crystal-clear sound with the Ari Solo Bas Wireless Headphones",
+  bgColor: "#f42c37",
+};
 
 const App = () => {
   return (
@@ -13,6 +27,7 @@ const App = () => {
       <CategoryRow1 />
       <CategoryRow2 />
       <Services />
+      <Banner data={{...BannerData}} />
     </div>
   );
 };
